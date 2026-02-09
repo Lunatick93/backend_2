23ed12be611e59dc9e43a81fc1c112ecfd32b90d
Nicolas Esteban Quispe <nickoesteban93@gmail.com>
2026-02-09 16:42:34 -0300

    update para los middleware de routes para roles de admin y user

--- src/routes/carts.router.js
+++ src/routes/carts.router.js
@@ -1,14 +1,18 @@
 import { Router } from "express";
 import * as ctrl from "../controllers/cart.controller.js";
+import { getCurrent } from "../middlewares/auth.middleware.js";
+import { requireUser } from "../middlewares/authorization.middleware.js";
 
 const router = Router();
 
-router.post("/", ctrl.create);
+// Cualquier usuario puede ver su carrito
 router.get("/:cid", ctrl.getById);
-router.post("/:cid/product/:pid", ctrl.addProduct);
-router.delete("/:cid/products/:pid", ctrl.deleteProduct);
-router.put("/:cid", ctrl.updateCart);
-router.put("/:cid/products/:pid", ctrl.updateQuantity);
-router.delete("/:cid", ctrl.clear);
+
+// Solo USER: agregar productos al carrito
+router.post("/:cid/product/:pid", getCurrent, requireUser, ctrl.addProduct);
+router.delete("/:cid/products/:pid", getCurrent, requireUser, ctrl.deleteProduct);
+router.put("/:cid", getCurrent, requireUser, ctrl.updateCart);
+router.put("/:cid/products/:pid", getCurrent, requireUser, ctrl.updateQuantity);
+router.delete("/:cid", getCurrent, requireUser, ctrl.clear);
 
 export default router;
--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -1,12 +1,17 @@
 import { Router } from "express";
 import * as ctrl from "../controllers/product.controller.js";
+import { getCurrent } from "../middlewares/auth.middleware.js";
+import { requireAdmin } from "../middlewares/authorization.middleware.js";
 
 const router = Router();
 
+// Cualquier usuario puede ver productos
 router.get("/", ctrl.getAll);
 router.get("/:pid", ctrl.getById);
-router.post("/", ctrl.create);
-router.put("/:pid", ctrl.update);
-router.delete("/:pid", ctrl.remove);
+
+// Solo ADMIN: crear, actualizar, eliminar productos
+router.post("/", getCurrent, requireAdmin, ctrl.create);
+router.put("/:pid", getCurrent, requireAdmin, ctrl.update);
+router.delete("/:pid", getCurrent, requireAdmin, ctrl.remove);
 
 export default router;
